b10ef65941eda597c8e995e70ccd83495b833111
nasium <nasium@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-05-24 23:44:22 +0000

    git-svn-id: https://plugins.svn.wordpress.org/itunes-affiliate-pro/trunk@548762 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- itunes-affiliate-pro/itunes-affiliate-pro.php
+++ itunes-affiliate-pro/itunes-affiliate-pro.php
@@ -18,23 +18,20 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 Plugin Name: iTunes Affiliate Pro
 Plugin URI: http://wordpress.org/extend/plugins/itunes-affiliate-pro/
 Description: Automatically adds iTunes affiliate links to content.
-Version: 1.0
+Version: 1.1
 Author: Jorge A. Gonzalez
 Author URI: http://www.foxybay.com
 License: GPL2
 */
 
 
-if ( get_option("IAP-isactive") ) add_filter( 'the_content', 'IAP_ParseText', 99 );
-
+if ( get_option("IAP-isactive") AND get_option("IAP-affiliate-code") ) {
+    add_filter( 'the_content', 'IAP_ParseText', 99 );
+}
 
 function IAP_ParseText( $text ) {
     
-    if (get_option("IAP-affiliate-code"))  {
-        $IAP_affiliate_URL = 'http://click.linksynergy.com/fs-bin/stat?id='.get_option("IAP-affiliate-code").'&offerid=146261&type=3&subid=0&tmpid=1826&RD_PARM1=';
-    } else {
-        $IAP_affiliate_URL = 'http://click.linksynergy.com/fs-bin/stat?id=G/REHtk3*ZI&offerid=146261&type=3&subid=0&tmpid=1826&RD_PARM1=';        
-    } 
+  $IAP_affiliate_URL = 'http://click.linksynergy.com/fs-bin/stat?id='.get_option("IAP-affiliate-code").'&offerid=146261&type=3&subid=0&tmpid=1826&RD_PARM1='; 
     
     preg_match_all("/<a href=\"http:\/\/itunes.apple.com\/us\/app\/(.*?)\".*?>(.*?)<\/a>/i", $text, $matches); 
     for($i=0;$i<count($matches[0]);$i++) { 
@@ -67,8 +64,8 @@ function IAP_settings_page() { ?>
     <div class="wrap">
         <h2>iTunes Affiliate Pro Options</h2>
         
-        <p>To start making money, you need to create a free LinkShare account. <a href="http://click.linksynergy.com/fs-bin/click?id=G/REHtk3*ZI&offerid=7097&type=3&subid=0" target="_blank"><strong>Create LinkShare Account Here</strong></a></p>
-        <p>Once you <a href="http://click.linksynergy.com/fs-bin/click?id=G/REHtk3*ZI&offerid=7097&type=3&subid=0" target="_blank">create a LinkShare account</a>, you will need to apply for the iTunes affiliate program. It takes seconds to apply and you will be approved within minutes. <a href="http://cli.linksynergy.com/cli/publisher/programs/advertiser_detail.php?oid=146261&mid=13508&offerNid=1" target="_blank">Click Here</a> for more information and to apply.</p>
+        <p>To get started you need a free LinkShare account. <a href="http://goo.gl/HOVzV" target="_blank"><strong>Create LinkShare Account</strong></a></p>
+        <p>Once you <a href="http://goo.gl/HOVzV" target="_blank">create a LinkShare account</a>, you will need to apply for the iTunes affiliate program.<br />It takes seconds to apply and you will be approved within minutes. <a href="http://goo.gl/9e8Ls" target="_blank">Click Here to Apply</a></p>
             
             <form method="post" action="options.php"> 
                 <?php 
@@ -78,7 +75,7 @@ function IAP_settings_page() { ?>
                 <table class="form-table">
                     <tr> 
                     <td nowrap valign="top" /><strong>LinkShare Affiliate ID</strong></th>
-                    <td width="100%"><input type="text" name="IAP-affiliate-code" value="<?php echo get_option('IAP-affiliate-code'); ?>" size="50" /><br /><a href="http://www.goldencan.com/help/Linkshare.aspx" target="_blank">Learn how to get your LinkShare Affiliate ID</a> / Example: <em>G/REHtk3*ZI</em></td>
+                    <td width="100%"><input type="text" name="IAP-affiliate-code" value="<?php echo get_option('IAP-affiliate-code'); ?>" size="50" /><br /><a href="http://goo.gl/TpcN4" target="_blank">Learn how to get your LinkShare Affiliate ID</a> / Example: <em>G/REHtk3*ZI</em></td>
                     </tr>  
                     <tr>
                     <td nowrap /><strong>Activate Filter</strong></th>
--- itunes-affiliate-pro/readme.txt
+++ itunes-affiliate-pro/readme.txt
@@ -3,7 +3,7 @@ Contributors: nasium
 Tags: itunes, affiliate, affiliate marketing, app store, ads, affiliate plugin, earn money, linkshare, posts, post, page, pages
 Requires at least: 3.0
 Tested up to: 3.3.2
-Stable tag: 1.0
+Stable tag: 1.1
 Author URI: http://www.foxybay.com/
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
