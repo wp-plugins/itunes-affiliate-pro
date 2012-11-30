4a80b9032432e1d7e4ee810c8610ef1ba90f86a6
nasium <nasium@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-11-30 21:45:38 +0000

    Bug fixes and new version.
    git-svn-id: https://plugins.svn.wordpress.org/itunes-affiliate-pro/tags/1.3@632432 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ itunes-affiliate-pro.php
@@ -0,0 +1,161 @@
+<?php
+/*  
+Copyright 2012  Jorge A. Gonzalez  (email : jorge@zeropaid.com)
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License, version 2, as 
+published by the Free Software Foundation.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+Plugin Name: iTunes Affiliate Pro
+Plugin URI: http://wordpress.org/extend/plugins/itunes-affiliate-pro/
+Description: Automatically adds iTunes affiliate links to content.
+Version: 1.3
+Author: Jorge A. Gonzalez
+Author URI: http://www.foxybay.com
+License: GPL2
+*/
+
+$IAP_affiliate_URL = 'http://click.linksynergy.com/fs-bin/stat?id='.get_option("IAP-affiliate-code").'&offerid=146261&type=3&subid=0&tmpid=1826&RD_PARM1='; 
+
+
+
+if ( get_option("IAP-isactive") AND get_option("IAP-affiliate-code") ) {
+    add_filter( 'the_content', 'IAP_ParseText', 99 );
+} 
+
+if ( !get_option("IAP-affiliate-code") ) {
+   add_action('admin_notices', 'IAP_notice');
+}
+
+add_action('admin_menu', 'IAP_create_menu');
+
+function IAP_create_menu() { 
+    add_options_page('iTunes Affiliate Pro Options', 'iTunes Affiliate Pro', 'manage_options', 'IAP_menu', 'IAP_settings_page'); 
+	add_action( 'admin_init', 'IAP_register' );
+}
+ 
+function IAP_register() {
+    add_option("IAP-affiliate-code", "");
+    add_option("IAP-isactive", "");
+	register_setting( 'IAP-settings', 'IAP-affiliate-code' ); 
+	register_setting( 'IAP-settings', 'IAP-isactive' ); 
+}
+
+function IAP_settings_page() { ?>
+    <div class="wrap">
+        <div id="icon-upload" class="icon32"></div> 
+        <h2>iTunes Affiliate Pro Options</h2> 
+        
+        
+                <div class="metabox-holder"> 
+                        <div class="postbox gdrgrid frontleft"> 
+                            <h3 class="hndle"><span>About iTunes Affiliate Pro </span></h3>
+                            <div class="gdsrclear"></div>
+                                <div class="inside">
+                                
+                                    <p>iTunes Affiliate Pro creates affiliate links inside your posts/pages.<br />No need to create LinkShare affiliate links inside your posts, this plugin does all the work.<br />Developed and maintained by <a href="http://www.thebutton.com" target="_blank">Jorge A. Gonzalez</a></p>
+                                    <a href="https://twitter.com/TheRealJAG" class="twitter-follow-button" data-show-count="false">Follow @TheRealJAG</a></p>                                      
+                                     <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+                                                                        
+                                </div>
+                        </div>
+                    </div>
+                    
+                    
+        
+                <div class="metabox-holder"> 
+                        <div class="postbox gdrgrid frontleft"> 
+                            <h3 class="hndle"><span>Getting Started </span></h3>
+                            <div class="gdsrclear"></div>
+                                <div class="inside">
+                                 <p>To get started you need a free LinkShare account. <br /><br /><input type="submit" class="button" value="Create LinkShare Account" onClick="window.open('http://goo.gl/HOVzV','external')" /></p>
+                                 <p>Once you <a href="http://goo.gl/HOVzV" target="_blank">create a LinkShare account</a>, you will need to apply for the iTunes affiliate program.<br />It takes seconds to apply and you will be approved within minutes. <a href="http://goo.gl/9e8Ls" target="_blank">Click Here to Apply</a></p>       
+                                                             
+                                </div>
+                        </div>
+                    </div>
+                    
+        
+                <div class="metabox-holder"> 
+                        <div class="postbox gdrgrid frontleft"> 
+                            <h3 class="hndle"><span>Install iTunes Affiliate Pro </span></h3>
+                            <div class="gdsrclear"></div>
+                                <div class="inside"> 
+                                    <form method="post" action="options.php"> 
+                                        <?php 
+                                        settings_fields('IAP-settings'); 
+                                        if( get_option("IAP-isactive") ){ $checked = "checked='checked'"; }  else { $checked = ""; }   
+                                        ?>
+                                        <table class="form-table">
+                                            <tr> 
+                                            <td nowrap valign="top" /><strong>LinkShare Affiliate ID</strong></th>
+                                            <td width="100%"><input type="text" name="IAP-affiliate-code" value="<?php echo get_option('IAP-affiliate-code'); ?>" size="50" /><br /><a href="http://goo.gl/TpcN4" target="_blank">Learn how to get your LinkShare Affiliate ID</a> / Example: <em>G/REHtk3*ZI</em></td>
+                                            </tr>  
+                                            <tr>
+                                            <td nowrap /><strong>Activate Filter</strong></th>
+                                            <td width="100%"><input type="checkbox" name="IAP-isactive" <?=$checked;?> /> Once you're ready to start adding affiliate links to your content click this box.</td>
+                                            </tr>  
+                                        </table>
+                                        <p class="submit"><input type="submit" class="button-primary" value="<?php _e('Save Changes') ?>" /></p>        
+                                    </form>                                 
+                                </div>
+                        </div>
+                    </div>
+                    
+                    
+        
+                <div class="metabox-holder"> 
+                        <div class="postbox gdrgrid frontleft"> 
+                            <h3 class="hndle"><span>Support</span></h3>
+                            <div class="gdsrclear"></div>
+                                <div class="inside"> 
+                                If you're having problems with the plugin or need help getting started, visit the <a href="http://wordpress.org/support/plugin/itunes-affiliate-pro" target="_blank">support forum</a>.                           
+                                </div>
+                        </div>
+                    </div>
+                    
+            
+    </div>
+<?php } 
+
+ 
+
+function IAP_notice(){
+    echo '<div class="error">
+       <p><a href="/wp-admin/options-general.php?page=IAP_menu">iTunes Affiliate Pro</a> requires a free LinkShare account. <input type="submit" class="button" value="Create Account" onClick="window.open(\'http://goo.gl/HOVzV\',\'external\')"></p>
+    </div>';
+}
+
+
+function IAP_ParseText( $text ) {
+    global $IAP_affiliate_URL;
+    
+    preg_match_all("/<a href=\"(.*?):\/\/itunes.apple.com\/(.*?)\".*?>(.*?)<\/a>/i", $text, $matches); 
+    
+    for($i=0;$i<count($matches[0]);$i++) { 
+      if(!preg_match("/rel=[\"\']*external nofollow[\"\']*/",$matches[0][$i])) { 
+        
+        preg_match_all("/<a.*? href=\"(.*?)\"(.*?)>(.*?)<\/a>/i", $matches[0][$i], $matches1);   
+            $text = str_replace($matches1[1][0],$IAP_affiliate_URL.urlencode($matches1[1][0]),$text);
+            $text = str_replace(">".$matches1[3][0]."</a>"," target='_blank' rel='external nofollow'>".$matches1[3][0]."</a>",$text);
+            
+            
+            
+            
+       }
+     } 
+return $text;
+}
+
+
+
+?>
---
+++ readme.txt
@@ -0,0 +1,68 @@
+=== iTunes Affiliate Pro ===
+Contributors: nasium
+Tags: itunes, affiliate, affiliate marketing, app store, ads, affiliate plugin, earn money, linkshare, posts, post, page, pages, associate, paypal, shop, shopping, marketing, plugin, url, apps, music, downloads
+Requires at least: 3.0
+Tested up to: 3.4
+Stable tag: 1.3
+Author URI: http://www.foxybay.com/
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Earn money effortlessly from your conent with iTunes Affiliate Pro.
+
+== Description ==
+
+iTunes Affiliate Pro creates affiliate links inside your posts/pages. No need to create LinkShare affiliate links inside your posts, this plugin does all the work and makes YOU money!
+
+**How does it work?**
+
+Before WordPress outputs a post, page, or custom post type; iTunes Affiliate Pro searches for a particular URL ('http://itunes.apple.com/us/album/...') string. If a match is found iTunes Affiliate Pro rewrites the link URL and adds an iTunes affiliate URL.
+
+iTunes Affiliate Pro can start making you money immediately without making any changes to your existing content.
+
+**Getting Started**
+
+Once you have configured iTunes Affiliate Pro, try adding the following HTML to a post. &lt;a href=&quot;http://itunes.apple.com/us/album/taxman/id401136641?i=401136642&quot;&gt;Taxman - Revolver&lt;/a&gt;
+
+If you have configured the plugin correctly and checked the `Activate Filter` checkbox you will see an affiliate URL in place of the iTunes URL.
+
+**Working Example**
+
+http://www.zeropaid.com/news/100885/top-5-iphone-podcast-apps/
+
+**Affiliate Program Details**
+
+The LinkShare program pays affiliates 5% of all purchases made within 3 days of clicking on one of your affiliate links. Assuming the visitor doesn't click on another affiliate link, you receive commissions on all purchases the user makes in the iTunes store. Since most new iPhone & iPad users go on a downloading frenzy after initially purchasing a new device, you can quickly make money using your WordPress site. 
+
+Reporting takes around 3-4 days to show up in LinkShare. If you don't see any sales after that period you'll want to make sure you have setup iTunes Affiliate Pro correctly. 
+
+**Support**
+
+If you're having problems with the plugin or need help getting started, visit the support forum: http://wordpress.org/support/plugin/itunes-affiliate-pro
+
+== Installation ==
+
+1. Upload the folder `itunes-affiliate-pro` to the `/wp-content/plugins/` directory
+1. Activate the plugin through the 'Plugins' menu in WordPress
+1. Visit the `iTunes Affiliate Pro Options` page under settings and add the necessary information.
+
+== Changelog ==
+
+= 1.3 =
+* Added support for all iTunes objects.
+* Added support for international affiliates.
+
+= 1.2 =
+* Removed extra spaces in plugin that broke RSS feeds.
+
+= 1.1 =
+* Small fixes to admin pages.
+
+= 1.0 =
+* Plugin added to the WordPress Plugin Repository
+ 
+== Upgrade Notice ==
+
+= 1.0 =
+Upgrade from internal dev
+ 
